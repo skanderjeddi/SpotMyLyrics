86ed1395e94705f254968ba482cb21362e5b85fd
Skander Jeddi <skander.jeddi2@gmail.com>
2020-09-16 13:12:25 +0200

    Added theoretical support for Linux

--- src/com/skanderj/spotmylyrics/SpotMyLyrics.java
+++ src/com/skanderj/spotmylyrics/SpotMyLyrics.java
@@ -33,7 +33,7 @@ public final class SpotMyLyrics {
 	public static final String GITHUB_URL = "https://github.com/skanderjeddi";
 
 	// Enable/disable verbose
-	public static final boolean VERBOSE = true;
+	public static final boolean VERBOSE = false;
 
 	// The base URL used to fetch lyrics
 	public static final String AZLYRICS_URL = "https://www.azlyrics.com/lyrics/%s/%s.html";
@@ -279,9 +279,11 @@ public final class SpotMyLyrics {
 
 	public String[] querySpotify() {
 		if (SpotMyLyrics.OS.contains("win")) {
-			return this.querySpotify_WIN();
+			return this.querySpotiftWINDOWS();
 		} else if (SpotMyLyrics.OS.contains("mac os")) {
-			return this.querySpotify_MACOS();
+			return this.querySpotifyMACOS();
+		} else if (SpotMyLyrics.OS.contains("linux")) {
+			return this.querySpotifyLINUX();
 		} else {
 			return null;
 		}
@@ -303,7 +305,13 @@ public final class SpotMyLyrics {
 		return output;
 	}
 
-	public String[] querySpotify_WIN() {
+	/**
+	 * Gets the currently playing song on Spotify. Relies on a Python script
+	 * included in /scripts/.
+	 *
+	 * @return (artist, track, strippedArtist, strippedTitle)
+	 */
+	public String[] querySpotiftWINDOWS() {
 		if (SpotMyLyrics.VERBOSE) {
 			System.out.printf("Querying Spotify for the current song...\n");
 		}
@@ -323,12 +331,11 @@ public final class SpotMyLyrics {
 	}
 
 	/**
-	 * !!! ONLY WORKS ON macOS !!! Gets the currently playing song on Spotify.
-	 * Relies on AppleScript.
+	 * Gets the currently playing song on Spotify. Relies on AppleScript.
 	 *
 	 * @return (artist, track, strippedArtist, strippedTitle)
 	 */
-	public String[] querySpotify_MACOS() {
+	public String[] querySpotifyMACOS() {
 		if (SpotMyLyrics.VERBOSE) {
 			System.out.printf("Querying Spotify for the current song...\n");
 		}
@@ -338,6 +345,31 @@ public final class SpotMyLyrics {
 			answer = this.readProcessOutput(appleScriptArgs);
 		} catch (final Exception exception) {
 			System.err.println("An error occurred while querying Spotify for the current song: " + exception.getMessage());
+			return null;
+		}
+		return this.formatAnswer(answer);
+	}
+
+	/**
+	 * Gets the currently playing song on Spotify. Relies on a Python script
+	 * included in /scripts/.
+	 *
+	 * @return (artist, track, strippedArtist, strippedTitle)
+	 */
+	public String[] querySpotifyLINUX() {
+		if (SpotMyLyrics.VERBOSE) {
+			System.out.printf("Querying Spotify for the current song...\n");
+		}
+		String answer = SpotMyLyrics.EMPTY;
+		try {
+			final String[] pythonScriptArgs = { "python", "./scripts/getspotifyinfolinux.py" };
+			answer = this.readProcessOutput(pythonScriptArgs);
+		} catch (final Exception exception) {
+			System.err.println("An error occurred while querying Spotify for the current song: " + exception.getMessage());
+			return null;
+		}
+		if (answer.equals("PAUSED") || answer.equals("NOT RUNNING")) {
+			System.err.println("Spotify is either paused or not running, can't identify the current track!");
 			return null;
 		}
 		return this.formatAnswer(answer);
