34ee5817e8deb236d63ecb6f9975f180dc2ef3f8
Skander Jeddi <skander.jeddi2@gmail.com>
2020-09-16 13:20:25 +0200

    Added a few comments

--- src/com/skanderj/spotmylyrics/SpotMyLyrics.java
+++ src/com/skanderj/spotmylyrics/SpotMyLyrics.java
@@ -127,6 +127,10 @@ public final class SpotMyLyrics {
 		return length;
 	}
 
+	/**
+	 * @return a String converting the raw @param bytes into human readable, SI
+	 *         format.
+	 */
 	public String humanReadableByteCountSI(long bytes) {
 		if ((-1000 < bytes) && (bytes < 1000)) {
 			return bytes + " B";
@@ -277,6 +281,9 @@ public final class SpotMyLyrics {
 		return source.substring(warningIndex + SpotMyLyrics.TARGET_WARNING.length(), endDivTagIndex).strip();
 	}
 
+	/**
+	 * @return a (artist, track) pair if found.
+	 */
 	public String[] querySpotify() {
 		if (SpotMyLyrics.OS.contains("win")) {
 			return this.querySpotiftWINDOWS();
@@ -289,6 +296,13 @@ public final class SpotMyLyrics {
 		}
 	}
 
+	/**
+	 * Runs a script through the underlying system.
+	 * 
+	 * @param args
+	 * @return the process' output
+	 * @throws Exception
+	 */
 	public String readProcessOutput(final String[] args) throws Exception {
 		String output = new String();
 		final ProcessBuilder processBuilder = new ProcessBuilder(args);
@@ -375,6 +389,12 @@ public final class SpotMyLyrics {
 		return this.formatAnswer(answer);
 	}
 
+	/**
+	 * Formats the script's output pair (artist, track)
+	 * 
+	 * @param answer
+	 * @return { formatted artist name, formatted track name, stripped artist name, stripped track name }
+	 */
 	public String[] formatAnswer(String answer) {
 		final String originalAnswer = new StringBuilder(answer).toString();
 		// If we have aliases setup
